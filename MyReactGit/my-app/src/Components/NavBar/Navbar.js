6916a869a6dcaaeb355da486dcb8013e62caca3a
Lazarus <papialazarus@gmail.com>
2023-04-20 19:40:09 +0200

    Animations

--- MyReactGit/my-app/src/Components/NavBar/Navbar.js
+++ MyReactGit/my-app/src/Components/NavBar/Navbar.js
@@ -1,13 +1,13 @@
 import './Navbar.css';
-import { Link } from "react-router-dom"
+import { Link } from 'react-scroll';
 
 export default function Navbar() {
     return (
       <div className="navbar">
         <ul>
-            <li><Link to="/">Intro</Link></li>
-            <li><Link to="/">Skills</Link></li>
-            <li><Link to="/">Contact</Link></li>
+            <li><Link to="intro" smooth={true} offset={-100} duration={500} >Intro</Link></li>
+            <li><Link to="skills-main" smooth={true} offset={-300} duration={500} >Project</Link></li>
+            <li><Link to="social-main" smooth={true} offset={-100} duration={500}>Contact</Link></li>
         </ul>
         </div>
     )
