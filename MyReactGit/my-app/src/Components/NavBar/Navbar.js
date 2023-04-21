f400574d76b976e6a8ba9fa1238a9dc42b8c12f7
Lazarus <papialazarus@gmail.com>
2023-04-21 16:30:07 +0200

    Fixed Responsiveness

--- MyReactGit/my-app/src/Components/NavBar/Navbar.js
+++ MyReactGit/my-app/src/Components/NavBar/Navbar.js
@@ -3,12 +3,14 @@ import { Link } from 'react-scroll';
 
 export default function Navbar() {
     return (
-      <div className="navbar">
-        <ul>
-            <li><Link to="intro" smooth={true} offset={-100} duration={500} >Intro</Link></li>
-            <li><Link to="skills-main" smooth={true} offset={-300} duration={500} >Project</Link></li>
-            <li><Link to="social-main" smooth={true} offset={-100} duration={500}>Contact</Link></li>
-        </ul>
+        <div className='navbar-container'>
+            <div className="navbar">
+                <ul>
+                    <li><Link to="intro" smooth={true} offset={-100} duration={500} >Intro</Link></li>
+                    <li><Link to="skills-main" smooth={true} offset={-300} duration={500} >Project</Link></li>
+                    <li><Link to="social-main" smooth={true} offset={-100} duration={500}>Contact</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
